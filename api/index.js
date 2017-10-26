baa143ff0771d69d557a3cf26373b334f485d43a
Luke McGregor <lukemcgregor@users.noreply.github.com>
2017-10-26 22:11:22 +1300

    postgres things

--- api/index.js
+++ api/index.js
@@ -1,64 +1,75 @@
 const express = require('express');
-const request = require('request');
-const mongo = require('mongodb');
+// const request = require('request');
+const pgtools = require('pgtools');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
-const mongoUrl = 'mongodb://localhost:27017/startracker';
 
 
-mongo.MongoClient.connect(mongoUrl, (err, db) => {
-  db.collection('tracked-repos').createIndex({ username: 1, repo: 1 }, { unique: true }, () => {
-    db.close();
-  });
+// This can also be a connection string
+// (in which case the database part is ignored and replaced with postgres)
+
+const config = {
+  user: 'postgres',
+  password: 'postgres',
+  port: 5432,
+  host: 'localhost'
+};
+
+pgtools.createdb(config, 'test-db', (err, res) => {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(res);
 });
 
 const app = express();
 
-app.post('/repos/:username/:repository', (req, res) => {
-  request({
-    url: `https://api.github.com/repos/${req.params.username}/${req.params.repository}`,
-    headers: {
-      'User-Agent': 'node'
-    }
-  }, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      mongo.MongoClient.connect(mongoUrl, (err, db) => {
-        db.collection('tracked-repos').insertOne({
-          username: req.params.username,
-          repo: req.params.repository
-        }, () => {
-          db.close();
-        });
-      });
-      res.send(`${JSON.parse(body).stargazers_count}`);
-    } else {
-      res.send(`${error || response.statusCode}`);
-    }
-  });
-});
+// app.post('/repos/:username/:repository', (req, res) => {
+//   request({
+//     url: `https://api.github.com/repos/${req.params.username}/${req.params.repository}`,
+//     headers: {
+//       'User-Agent': 'node'
+//     }
+//   }, (error, response, body) => {
+//     if (!error && response.statusCode === 200) {
+//       mongo.MongoClient.connect(mongoUrl, (err, db) => {
+//         db.collection('tracked-repos').insertOne({
+//           username: req.params.username,
+//           repo: req.params.repository
+//         }, () => {
+//           db.close();
+//         });
+//       });
+//       res.send(`${JSON.parse(body).stargazers_count}`);
+//     } else {
+//       res.send(`${error || response.statusCode}`);
+//     }
+//   });
+// });
 
-app.get('/repos/:username/:repository/star-history', (req, res) => {
-  mongo.MongoClient.connect(mongoUrl, (err, db) => {
-    db.collection('repo-stars')
-      .find({
-        username: req.params.username,
-        repo: req.params.repository
-      })
-      .sort({ _id: 1 })
-      .toArray((err, history) => {
-        if (history) {
-          res.send(history.map(h => ({
-            timestamp: mongo.ObjectId(h._id).getTimestamp(),
-            stargazersCount: h.stargazers_count
-          })));
-        } else {
-          res.status(404).send('Not found');
-        }
-        db.close();
-      });
-  });
-});
+// app.get('/repos/:username/:repository/star-history', (req, res) => {
+//   mongo.MongoClient.connect(mongoUrl, (err, db) => {
+//     db.collection('repo-stars')
+//       .find({
+//         username: req.params.username,
+//         repo: req.params.repository
+//       })
+//       .sort({ _id: 1 })
+//       .toArray((err, history) => {
+//         if (history) {
+//           res.send(history.map(h => ({
+//             timestamp: mongo.ObjectId(h._id).getTimestamp(),
+//             stargazersCount: h.stargazers_count
+//           })));
+//         } else {
+//           res.status(404).send('Not found');
+//         }
+//         db.close();
+//       });
+//   });
+// });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
